ef91d9c04535496b7b112636c667064817e1ef67
artem remnev <tearrema@gmail.com>
2018-06-05 14:32:58 +0300

    Classes and DataBase

---
+++ Job.Classes/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Classes
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Ubiversity { get; set; }
+        public string Faculty { get; set; }
+        public string Login { get; set; }
+        public List<Specialization> sp { get; set; }
+        public string Password { get; set; }
+    }
+}
