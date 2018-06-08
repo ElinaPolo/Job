5224c7811a037b7ec412d094fb3fde7285f84ef0
Элина Половицкая <ElinaP98@gmail.com>
2018-06-08 22:18:17 +0700

    s

--- Team_Project/AddNewEmployee.xaml.cs
+++ Team_Project/AddNewEmployee.xaml.cs
@@ -31,6 +31,7 @@ namespace Team_Project
         private void Register_Click(object sender, RoutedEventArgs e)
         {
             var y = int.Parse(textBoxYear.Text);
+
             repository.SaveEmployee(textBoxFullname.Text, textBoxLogin.Text, PasswordBox.Password, textBoxEducation.Text, comboBoxSpecialization.SelectedItem as Specialization, comboBoxGrade.SelectedItem as Grades);
             NavigationService.Navigate(new LoginPage(repository, false));
         }
