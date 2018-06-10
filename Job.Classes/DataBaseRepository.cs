701e12edf43c1b88f7825ad68cb51230aa7aa2cb
artem remnev <tearrema@gmail.com>
2018-06-10 13:39:42 +0300

    f

--- Job.Classes/DataBaseRepository.cs
+++ Job.Classes/DataBaseRepository.cs
@@ -1,13 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Job.Classes
 {
-    public class DataBaseRepository:IRepository
+    public class DataBaseRepository : IRepository
     {
-      //  public Employee Register(string login, string password, string name,string education, )
+        //  public Employee Register(string login, string password, string name,string education, )
+        public static void SaveEmployee(string name, string login, string password, string education, Specialization specialization, Grades grade)
+        {
+
+            Employee employee = new Employee()
+            {
+                Name = name,
+                Login = login,
+                Password = GetHash(password),
+                Education = education,
+                Specializations = specialization,
+                Grade = grade
+            };
+            using (var context = new Context())
+            {
+                context.Employee_.Add(employee);
+                context.SaveChanges();
+            }
+        }
+        public static void SaveEmployer(string nameofcompany, string login, string password)
+        {
+
+            Employer employer = new Employer()
+            {
+                NameOfTheCompany = nameofcompany,
+                Login = login,
+                Password = GetHash(password)
+            };
+            using (var context = new Context())
+            {
+                context.Employer_.Add(employer);
+                context.SaveChanges();
+            }
+        }
+        public static void AddVacancy(Employer employer, string vacancyname, string salary, string adress, string number, string contactperson, Specialization sp)
+        {
+            using (var context = new Context())
+            {
+                var vacanc = new Vacancy()
+                {
+                    VacancyName = vacancyname,
+                    Salary = salary,
+                    Address = adress,
+                    Number = number,
+                    ContactPerson = contactperson,
+                    Specialization = sp,
+                    Employer = context.Employer_.FirstOrDefault(m => m.Login == employer.Login)
+                };
+                if (employer.Vacancies == null)
+                {
+                    context.Employer_.FirstOrDefault(x => x.Login == employer.Login).Vacancies = new List<Vacancy>();
+                }
+                context.Vacancy_.Add(vacanc);
+                context.SaveChanges();
+            }
+
+        }
+        public static string GetHash(string password)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hash);
+        }
     }
 }
--- Job.Classes/Repository.cs
+++ Job.Classes/Repository.cs
@@ -55,12 +55,7 @@ namespace Job.Classes
                 }
             }
         }
-        public static string GetHash(string password)
-        {
-            var md5 = MD5.Create();
-            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
-            return Convert.ToBase64String(hash);
-        }
+        
         public static List<T> RestoreList<T>(string fileName)
         {
             using (var sr = new StreamReader(fileName))
@@ -72,57 +67,6 @@ namespace Job.Classes
                 }
             }
         }
-        public static void SaveEmployee(string name, string login, string password,string education, Specialization specialization , Grades grade )
-        {
-           
-            Employee employee = new Employee()
-            {
-                Name = name,
-                Login = login,
-                Password = GetHash(password),
-                Education=education,
-                Specializations=specialization,
-                Grade=grade
-            };
-            using (var context = new Context())
-            {
-                context.Employee_.Add(employee);
-                context.SaveChanges();
-            }           
-        }
-        public static void SaveEmployer(string nameofcompany, string login, string password)
-        {
-
-            Employer employer = new Employer()
-            {
-                NameOfTheCompany = nameofcompany,
-                Login = login,
-                Password = GetHash(password)
-            };
-            using (var context = new Context())
-            {
-                context.Employer_.Add(employer);
-                context.SaveChanges();
-            }
-        }
-        public static void AddVacancy(Employer employer, string vacancyname,string salary,string adress,string number,string contactperson,Specialization sp )
-        {
-            using (var context = new Context())
-            {
-                var vacanc = new Vacancy()
-                {
-                    VacancyName = vacancyname,
-                    //Salary = salary,
-                    Address = adress,
-                    Number = number,
-                    //ContactPerson = contactperson,
-                    Specialization = sp,
-                    Employer=context.Employer_.FirstOrDefault(m=>m.Login==employer.Login)
-                };
-                context.Vacancy_.Add(vacanc);
-                context.SaveChanges();
-            }
-                
-        }
+             
     }
 }
