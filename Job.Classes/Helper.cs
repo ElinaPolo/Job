32e46bb7fd8ee1b9872cc262e0b30720342d0a81
artem remnev <tearrema@gmail.com>
2018-06-10 14:18:34 +0300

    v

---
+++ Job.Classes/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Classes
+{
+   public class Helper
+    {
+        //public static void Employee(Grades grade, Specialization specialization)
+        //{
+        //    var emp = new List<Employee>();
+        //    using (var context = new Context())
+        //    {
+        //        if (grade != null)
+        //        {
+        //            foreach (var c in context.Employee_)
+        //            {
+        //                if (grade == c.Grade)
+        //                    emp.Add(c);
+        //            }
+        //        }
+
+        //        if (specialization != null)
+
+        //        {
+        //            foreach (var v in context.Employee_)
+        //            {
+        //                if(emp)
+
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
