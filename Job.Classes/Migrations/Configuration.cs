f024c8ec9529235765bca4576e1cbd064c95e12b
artem remnev <tearrema@gmail.com>
2018-06-11 23:37:44 +0300

    migration

---
+++ Job.Classes/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace Job.Classes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Job.Classes.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Job.Classes.Context";
+        }
+
+        protected override void Seed(Job.Classes.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
