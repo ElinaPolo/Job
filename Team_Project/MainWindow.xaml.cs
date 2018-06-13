72ff0b35877c4db4aedf5d034f289caffd111fa7
artem remnev <tearrema@gmail.com>
2018-06-14 02:34:59 +0300

    design of pages

--- Team_Project/MainWindow.xaml.cs
+++ Team_Project/MainWindow.xaml.cs
@@ -28,10 +28,7 @@ namespace Team_Project
             InitializeComponent();
 
             FirstWindow.Navigate(new Welcome());
-            using (var context = new Context())
-            {
-                var Spez = context.Grade_.ToList();               
-            }
+          
         }
     }
 }
