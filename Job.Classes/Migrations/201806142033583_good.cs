7a9698be52db13615ac9e408ea1fe98507585563
artem remnev <tearrema@gmail.com>
2018-06-14 23:34:12 +0300

    fix

---
+++ Job.Classes/Migrations/201806142033583_good.cs
@@ -0,0 +1,16 @@
+namespace Job.Classes.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class good : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
